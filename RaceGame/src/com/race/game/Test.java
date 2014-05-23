f229287fa5b519b9afad9a09e2af4bdd7d863bfc
Axel <User@User-PC>
2014-05-23 15:42:40 +0200

    test

---
+++ RaceGame/src/com/race/game/Test.java
@@ -0,0 +1,53 @@
+package com.race.game;
+
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
+
+public class Test {
+	//
+	//	// react to collision
+	//	if (collisionX)
+	//	{
+	//		setX(oldX);
+	//		touchpad.setX(0);
+	//	}
+	//	
+	//	if (collisionY)
+	//	{
+	//		setY(oldY);
+	//		touchpad.setY(0);
+	//	}
+	//}
+	//	
+	//	private boolean isCellBlocked(float x, float y) {
+	//		Cell cell = collisionLayer.getCell((int) (x / collisionLayer.getTileWidth()), (int) (y / collisionLayer.getTileHeight()));
+	//		return cell != null && cell.getTile() != null && cell.getTile().getProperties().containsKey("limit");
+	//	}
+	//
+	//	public boolean collidesRight() {
+	//		for(float step = 0; step < getHeight(); step += collisionLayer.getTileHeight() / 2)
+	//			if(isCellBlocked(getX() + getWidth(), getY() + step))
+	//				return true;
+	//		return false;
+	//	}
+	//
+	//	public boolean collidesLeft() {
+	//		for(float step = 0; step < getHeight(); step += collisionLayer.getTileHeight() / 2)
+	//			if(isCellBlocked(getX(), getY() + step))
+	//				return true;
+	//		return false;
+	//	}
+	//
+	//	public boolean collidesTop() {
+	//		for(float step = 0; step < getWidth(); step += collisionLayer.getTileWidth() / 2)
+	//			if(isCellBlocked(getX() + step, getY() + getHeight()))
+	//				return true;
+	//		return false;
+	//
+	//	}
+	//
+	//	public boolean collidesBottom() {
+	//		for(float step = 0; step < getWidth(); step += collisionLayer.getTileWidth() / 2)
+	//			if(isCellBlocked(getX() + step, getY()))
+	//				return true;
+	//		return false;
+}
